2f9ae412d11cbd22e5ee54cff11ddfb13e7a92cd
AayushSomwanshi21 <aayushsomwanshi21@gmail.com>
2024-08-22 15:30:21 +0530

    database connected

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const connectDB = require('./db');
+const express = require('express');
+const cors = require('cors')
+connectDB();
+const app = express();
+const port = 5000
+app.use(express.json());
+app.use(cors())
+app.get('/', (req, res) => res.send("Hello World"));
+
+//app.use('/api/auth', require('./routes/auth'));
+//app.use('/api/notes', require('./routes/notes'))
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
