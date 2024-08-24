501c98e44551300e41a6363fb1c03e12aeca4fb4
AayushSomwanshi21 <aayushsomwanshi21@gmail.com>
2024-08-24 15:05:16 +0530

    display all msgs

--- server/routes/messaging.js
+++ server/routes/messaging.js
@@ -2,11 +2,41 @@ const fetchuser = require('../middleware/fetchuser');
 const Message = require('../models/Message')
 const express = require('express');
 const router = express.Router();
+const { ObjectId } = require('mongodb')
+// GET all the chats of a user on 'http://localhost:5000/api/messaging/allchat/:id'
+router.get('/allchat', fetchuser, async (req, res) => {
+
+    try {
+        const allchats = await Message.aggregate([
+            {
+                $match: { senderId: new ObjectId(String(req.user.id)) }
+            },
+            {
+                $sort: { sentAt: 1 }
+            },
+            {
+                $group: {
+                    _id: "$receiverId",
+                    messages: { $push: "$$ROOT" }
+                }
+            }
+
+        ]);
+        //console.log(allchats)
+        return res.status(201).json({ allchats });
+
+    } catch (error) {
+        return res.status(400).json({ error: "Something went wrong" });
+    }
+
+});
+
 
 // Get a specific chat using GET on 'http://localhost:5000/api/messaging/getchat/:id'
 router.get('/getchat/:id', fetchuser, async (req, res) => {
 
     try {
+        // GET msgs sent by both users to each other
         const allmsgs = await Message.find({
             $or: [
                 { senderId: req.user.id, receiverId: req.params.id },
@@ -14,6 +44,7 @@ router.get('/getchat/:id', fetchuser, async (req, res) => {
             ]
         }).sort({ sentAt: 1 });
         return res.status(201).json({ allmsgs });
+
     } catch (error) {
         return res.status(400).json({ error: "Something went wrong" });
     }
@@ -24,12 +55,15 @@ router.get('/getchat/:id', fetchuser, async (req, res) => {
 router.post('/sendchat/:id', fetchuser, async (req, res) => {
 
     const receiverId = req.params.id;
-    console.log(req.params.id)
+    console.log(req.params.id);
+
     // senderId can be extracted from req.user.id
     const senderId = req.user.id;
     const { content } = req.body;
+
     //console.log(`senderId:${senderId}\nreceiverId:${receiverId}\ncontent:${content}`)
-    //console.log(req.body)
+    //console.log(req.body);
+
     if (!senderId || !receiverId || !content) {
         return res.status(400).json({ error: "Something went wrong in credentials" });
     }
